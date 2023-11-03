a6bf29ea4602b16047c96a49cac6171fa601fcb9
agungsurya <100355392+asurya-gz@users.noreply.github.com>
2023-11-03 09:03:41 +0700

    dsvsdv

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,7 @@
 export default function Navbar() {
-  return <div className="navbar h-[100px] w-full bg-gray-200">Ini Test Perubahan Ketiga</div>;
-  
-
-  
+  return (
+    <div className="navbar h-[100px] w-full bg-gray-200">
+      Ini Test Perubahan jgkgk
+    </div>
+  );
 }
